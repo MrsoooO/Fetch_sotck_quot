7085522159ab657c4e4463f2649e1921777f26ab
MrsooO <sjw_mrsooo@163.com>
2023-03-22 11:16:08 +0800

    修改

--- fetch_stock_quote_history.py
+++ fetch_stock_quote_history.py
@@ -2,8 +2,9 @@
 import baostock as bs
 import numpy as np
 import pandas as pd
-import mplfinance as mpl
-
+import mplfinance as mpf
+from inter_candles import InterCandle
+import matplotlib.pyplot as plt
 
 
 
@@ -69,13 +70,52 @@ def data_processing(data):
     return df
 def draw_k(df):
     # print(df)
+    my_color = mpf.make_marketcolors(up='r',
+                                     down='g',
+                                     edge='inherit',
+                                     wick='inherit',
+                                     volume='inherit')
+    my_style = mpf.make_mpf_style(marketcolors=my_color,
+                                  figcolor='(0.82, 0.83, 0.85)',
+                                  gridcolor='(0.82, 0.83, 0.85)')
+    normal_label_font = {'fontname': 'PingFang HK',
+                         'size': '11',
+                         'color': 'black',
+                         'va': 'bottom',
+                         'ha': 'right'}
+
     df['Date'] = pd.to_datetime(df['Date'])
     df.set_index(['Date'], inplace=True)
     df=df.astype(float)
     # print(df)
+    plot_data=df.iloc[100:200]
+    last_data=plot_data.iloc[-1]
+    fig=mpf.figure(style=my_style,figsize=(12,8),facecolor=(0.82,0.83,0.85))
+    ax1=fig.add_axes([0.06, 0.25, 0.88, 0.60])
+    ax2 = fig.add_axes([0.06, 0.15, 0.88, 0.10], sharex=ax1)
+    ax3 = fig.add_axes([0.06, 0.05, 0.88, 0.10], sharex=ax1)
+    ax1.set_ylabel('price')
+    ax2.set_ylabel('volume')
+    ax3.set_ylabel('macd')
+    fig.text(0.50, 0.94, 'sh.600000:')
+    fig.text(0.12, 0.89, 'Open/Close: ',**normal_label_font)
+    fig.text(0.14, 0.89, f'{np.round(last_data["Open"], 3)} / {np.round(last_data["Close"], 3)}')
+    fig.text(0.12, 0.86, f'{last_data.name.date()}')
+    fig.text(0.40, 0.90, 'High: ',**normal_label_font)
+    fig.text(0.40, 0.90, f'{last_data["High"]}')
+    fig.text(0.40, 0.86, 'Low: ',**normal_label_font)
+    fig.text(0.40, 0.86, f'{last_data["Low"]}')
+    fig.text(0.55, 0.90, 'Volum(M): ',**normal_label_font)
+    fig.text(0.55, 0.90, f'{np.round(last_data["Volume"] / 1000000, 3)}')
 
-    mpl.plot(df.loc[:],type='candle',ylabel='price',style='binance',title='test',
-             mav=(5,21),volume=True,figratio=(16,8),figscale=2,ylabel_lower='Volume',savefig=f'./{np.random.random(1)}.png')
+    mpf.plot(plot_data,
+             ax=ax1,
+             volume=ax2,
+             type='candle',
+             style=my_style)
+    fig.show()
+    # mpf.plot(plot_data,type='candle',ylabel='price',style=my_style,title='test',
+    #          mav=(5,21),volume=True,figratio=(16,8),figscale=2,ylabel_lower='Volume',savefig=f'./{np.random.random(1)}.png')
 
 def main():
     # producer=Kafka('./Config.ini').kafka_producer()
@@ -83,9 +123,22 @@ def main():
     # producer.send('test',"asdasd")
     #
     # producer.close()
-    data=get_stock_data("sh.600000",start_date='2023-01-01')
+    my_color = mpf.make_marketcolors(up='r',
+                                     down='g',
+                                     edge='inherit',
+                                     wick='inherit',
+                                     volume='inherit')
+    my_style = mpf.make_mpf_style(marketcolors=my_color,
+                                  figcolor='(0.82, 0.83, 0.85)',
+                                  gridcolor='(0.82, 0.83, 0.85)')
+    data=get_stock_data("sh.600000")
     df=data_processing(data)
-    draw_k(df)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index(['Date'], inplace=True)
+    df=df.astype(float)
+    candle= InterCandle(df,my_style)
+    candle.refresh_plot(150)
+    # print(plt.get_backend())
     #https://blog.csdn.net/Shepherdppz/article/details/117575286
 if __name__ == '__main__':
     main()
---
+++ inter_candles.py
@@ -0,0 +1,78 @@
+#encoding=utf-8
+import mplfinance as mpf
+import numpy as np
+import matplotlib.pyplot as plt
+class InterCandle:
+    def __init__(self, data, my_style):
+        self.data=data
+        self.style=my_style
+        self.idx_start=0
+        self.press=False
+        self.xpress=None
+        self.fig = mpf.figure(style=my_style, figsize=(12, 8), facecolor=(0.82, 0.83, 0.85),dpi=200)
+        fig = self.fig
+        self.ax1 = fig.add_axes([0.08, 0.25, 0.88, 0.60])
+        self.ax2 = fig.add_axes([0.08, 0.15, 0.88, 0.10], sharex=self.ax1)
+        self.ax1.set_ylabel('Price')
+        self.ax2.set_ylabel('Volume')
+        # self.t1= fig.text(0.50, 0.94, 'sh.600000:')
+        # self.t2= fig.text(0.12, 0.89, 'Open/Close: ', **normal_label_font)
+        # self.t3= fig.text(0.14, 0.89, '')
+        # self.t4= fig.text(0.12, 0.86, '')
+        # self.t5= fig.text(0.40, 0.90, 'High: ', **normal_label_font)
+        # self.t6= fig.text(0.40, 0.90, '')
+        # self.t7= fig.text(0.40, 0.86, 'Low: ', **normal_label_font)
+        # self.t8= fig.text(0.40, 0.86, '')
+        # self.t9= fig.text(0.55, 0.90, 'Volum(M): ', **normal_label_font)
+        # self.t10= fig.text(0.55, 0.90, '')
+        fig.canvas.mpl_connect('button_press_event', self.on_press)
+        fig.canvas.mpl_connect('button_release_event', self.on_release)
+        fig.canvas.mpl_connect('motion_notify_event', self.on_motion)
+
+    def refresh_plot(self,idx_start):
+        all_data = self.data
+        plot_data = all_data.iloc[idx_start:idx_start + 100]
+        mpf.plot(plot_data,
+                 ax=self.ax1,
+                 volume=self.ax2,
+                 type='candle',
+                 datetime_format='%Y-%m',
+                 xrotation=0)
+        mpf.switch_backend('agg')
+        self.fig.show()
+
+    def on_press(self,event):
+        if not event.inaxes ==self.ax1:
+            return
+        if event.button != 1:
+            return
+        self.pressed = True
+        self.xpress = event.xdata
+
+    def on_motion(self, event):
+        if not self.pressed:
+            return
+            # 如果移动出了ax1的范围，也退出处理函数
+        if not event.inaxes == self.ax1:
+            return
+            # 如果鼠标在ax1范围内，且左键按下，则开始计算dx，并根据dx计算新的K线图起点
+        dx = int(event.xdata - self.xpress)
+        new_start = self.idx_start - dx
+        if new_start <= 0:
+            new_start = 0
+        if new_start >= len(self.data) - 100:
+            new_start = len(self.data) - 100
+        self.ax1.clear()
+        self.ax2.clear()
+        self.refresh_plot(new_start)
+
+    def on_release(self, event):
+        # 按键释放后，设置鼠标的pressed为False
+        self.pressed = False
+        # 此时别忘了最后一次更新K线图的起点，否则下次拖拽的时候就不会从这次的起点开始移动了
+        dx = int(event.xdata - self.xpress)
+        self.idx_start -= dx
+        if self.idx_start <= 0:
+            self.idx_start = 0
+        if self.idx_start >= len(self.data) - 100:
+            self.idx_start = len(self.data) - 100
